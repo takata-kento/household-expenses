ce88a22c2947bfac4a879d40b09b92fd1aa4ca3f
TakataKento <114200801+takata-kento@users.noreply.github.com>
2025-07-30 19:59:21 +0900

    [api] format classes (#18)

--- household_expenses/src/test/java/com/takata_kento/household_expenses/TestcontainersConfiguration.java
+++ household_expenses/src/test/java/com/takata_kento/household_expenses/TestcontainersConfiguration.java
@@ -8,9 +8,10 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
-	}
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+    }
 }
--- household_expenses/src/test/java/com/takata_kento/household_expenses/domain/valueobject/MoneyTest.java
+++ household_expenses/src/test/java/com/takata_kento/household_expenses/domain/valueobject/MoneyTest.java
@@ -1,7 +1,8 @@
 package com.takata_kento.household_expenses.domain.valueobject;
 
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class MoneyTest {
 
