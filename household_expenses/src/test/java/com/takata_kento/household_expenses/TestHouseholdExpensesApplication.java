3aabb018632b5e2c253757c94f66ae91fea7e0da
takata-kento <takata_kento@ims-sol.co.jp>
2025-07-30 19:56:51 +0900

    refactor: format TestHouseholdExpensesApplication class

--- household_expenses/src/test/java/com/takata_kento/household_expenses/TestHouseholdExpensesApplication.java
+++ household_expenses/src/test/java/com/takata_kento/household_expenses/TestHouseholdExpensesApplication.java
@@ -3,7 +3,8 @@ package com.takata_kento.household_expenses;
 import org.springframework.boot.SpringApplication;
 
 public class TestHouseholdExpensesApplication {
-	public static void main(String[] args) {
-		SpringApplication.from(HouseholdExpensesApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.from(HouseholdExpensesApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 }
