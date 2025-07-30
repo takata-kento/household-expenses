948f11d696b5d24d5bf8cc1ea13b2ba29b418ddc
takata-kento <takata_kento@ims-sol.co.jp>
2025-07-30 19:48:45 +0900

    refactor: format HouseholdExpensesApplication class

--- household_expenses/src/main/java/com/takata_kento/household_expenses/HouseholdExpensesApplication.java
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/HouseholdExpensesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HouseholdExpensesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HouseholdExpensesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HouseholdExpensesApplication.class, args);
+    }
 }
