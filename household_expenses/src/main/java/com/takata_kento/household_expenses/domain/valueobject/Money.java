131ba1418fd4c2992dad0287557880b96fe286f7
takata-kento <takata_kento@ims-sol.co.jp>
2025-07-30 19:50:26 +0900

    refactor: format Money class

--- household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/Money.java
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/Money.java
@@ -1,7 +1,6 @@
 package com.takata_kento.household_expenses.domain.valueobject;
 
 public record Money(int amount) {
-
     public Money add(Money other) {
         return new Money(this.amount + other.amount);
     }
