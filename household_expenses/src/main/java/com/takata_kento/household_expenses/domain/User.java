cbe1aa92c29e3f564a841692cbd508aa013a7acf
takata-kento <takata_kento@ims-sol.co.jp>
2025-08-04 21:12:51 +0900

    feat: create User entity

---
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/domain/User.java
@@ -0,0 +1,36 @@
+package com.takata_kento.household_expenses.domain;
+
+import com.takata_kento.household_expenses.domain.valueobject.UserGroupId;
+import com.takata_kento.household_expenses.domain.valueobject.UserId;
+import com.takata_kento.household_expenses.domain.valueobject.Username;
+import java.util.Optional;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+public class User {
+
+    @Id
+    private final UserId id;
+
+    private final Username username;
+    private final UserGroupId userGroupId;
+
+    public User(UserId id, Username username, UserGroupId userGroupId) {
+        this.id = id;
+        this.username = username;
+        this.userGroupId = userGroupId;
+    }
+
+    public UserId id() {
+        return id;
+    }
+
+    public Username username() {
+        return username;
+    }
+
+    public Optional<UserGroupId> userGroupId() {
+        return Optional.ofNullable(userGroupId);
+    }
+}
