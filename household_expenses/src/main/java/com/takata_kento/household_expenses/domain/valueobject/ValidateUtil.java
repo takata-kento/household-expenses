38271ce91e773da1e0df1ee9008db627597b4121
takata-kento <tkt1996knt@gmail.com>
2025-12-16 19:08:36 +0900

    prettier change

--- household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/ValidateUtil.java
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/ValidateUtil.java
@@ -3,6 +3,7 @@ package com.takata_kento.household_expenses.domain.valueobject;
 import java.util.UUID;
 
 class ValidateUtil {
+
     static void validUUID(UUID value, Class<?> type) {
         if (value == null) {
             throw new IllegalArgumentException(type.getSimpleName() + " must not be null");
