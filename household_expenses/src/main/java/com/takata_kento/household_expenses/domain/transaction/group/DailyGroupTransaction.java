b80d34b01e14d76f25179cd9ccff6f181240df36
TakataKento <114200801+takata-kento@users.noreply.github.com>
2025-12-18 20:25:07 +0900

    [api] feat daily group transaction (#65)

---
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/domain/transaction/group/DailyGroupTransaction.java
@@ -0,0 +1,88 @@
+package com.takata_kento.household_expenses.domain.transaction.group;
+
+import com.takata_kento.household_expenses.domain.valueobject.DailyGroupTransactionId;
+import com.takata_kento.household_expenses.domain.valueobject.Description;
+import com.takata_kento.household_expenses.domain.valueobject.LivingExpenseCategoryId;
+import com.takata_kento.household_expenses.domain.valueobject.Money;
+import com.takata_kento.household_expenses.domain.valueobject.UserGroupId;
+import com.takata_kento.household_expenses.domain.valueobject.UserId;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("daily_group_transaction")
+public class DailyGroupTransaction {
+
+    @Id
+    @Column("id")
+    private DailyGroupTransactionId id;
+
+    @Column("user_group_id")
+    private UserGroupId userGroupId;
+
+    @Column("transaction_date")
+    private LocalDate transactionDate;
+
+    @MappedCollection(idColumn = "daily_group_transaction_id", keyColumn = "created_at")
+    private List<DailyLivingExpense> livingExpenses;
+
+    @Version
+    private Integer version;
+
+    public DailyGroupTransaction(
+        DailyGroupTransactionId id,
+        UserGroupId userGroupId,
+        LocalDate transactionDate,
+        List<DailyLivingExpense> livingExpenses,
+        Integer version
+    ) {
+        this.id = id;
+        this.userGroupId = userGroupId;
+        this.transactionDate = transactionDate;
+        this.livingExpenses = livingExpenses != null ? new ArrayList<>(livingExpenses) : new ArrayList<>();
+        this.version = version;
+    }
+
+    public DailyGroupTransactionId id() {
+        return this.id;
+    }
+
+    public UserGroupId userGroupId() {
+        return this.userGroupId;
+    }
+
+    public LocalDate transactionDate() {
+        return this.transactionDate;
+    }
+
+    public List<DailyLivingExpense> livingExpenses() {
+        return new ArrayList<>(this.livingExpenses);
+    }
+
+    public void addLivingExpense(
+        UserId userId,
+        LivingExpenseCategoryId livingExpenseCategoryId,
+        Money amount,
+        Description memo
+    ) {
+        DailyLivingExpense livingExpense = DailyLivingExpense.create(
+            this.id,
+            userId,
+            livingExpenseCategoryId,
+            amount,
+            memo
+        );
+        this.livingExpenses.add(livingExpense);
+    }
+
+    public Money calculateTotalLivingExpense() {
+        return this.livingExpenses.stream()
+            .map(DailyLivingExpense::amount)
+            .reduce(new Money(0), Money::add);
+    }
+}
