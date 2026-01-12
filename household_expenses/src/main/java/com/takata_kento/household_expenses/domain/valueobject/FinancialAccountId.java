91df4cb318968a5b02b13e326c3b6f388d6699dd
TakataKento <114200801+takata-kento@users.noreply.github.com>
2026-01-12 18:09:31 +0900

    Revert "[api] create entity balance edit history" (#74)

--- household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/FinancialAccountId.java
+++ household_expenses/src/main/java/com/takata_kento/household_expenses/domain/valueobject/FinancialAccountId.java
@@ -10,9 +10,4 @@ public record FinancialAccountId(UUID value) implements UUIDValueObject {
     public String toString() {
         return value.toString();
     }
-
-    @Override
-    public String toString() {
-        return value.toString();
-    }
 }
